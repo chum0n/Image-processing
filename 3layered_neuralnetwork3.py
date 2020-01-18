6e2c5ae76c97a1e7a9ad365cbc4593153633e197
tkt914 <cyumon_tkt.0914@icloud.com>
2020-01-18 19:22:01 +0900

    create functions.py

--- 3layered_neuralnetwork3.py
+++ 3layered_neuralnetwork3.py
@@ -2,6 +2,7 @@ import numpy as np
 from mnist import MNIST
 import matplotlib.pyplot as plt
 from pylab import cm
+from common.functions import *
 
 INNODES = 784
 HNODES = 100
@@ -30,27 +31,11 @@ def init_network():
 def innodes_cal(x):
     return x
 
-# シグモイド関数(活性化関数)
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
 # 中間層での入力->出力
 def hnodes_cal(yin, network):
     ah = np.dot(yin, network['Wh'].T) + network['bh'].T
     yh = sigmoid(ah)
     return yh
-
-# ソフトマックス関数
-def softmax(a):
-    # 一番大きい値を取得
-    c = np.max(a, axis=1, keepdims=True)
-    # 各要素から一番大きな値を引く（オーバーフロー対策）
-    exp_a = np.exp(a - c)
-    sum_exp_a = np.sum(exp_a, axis=1, keepdims=True)
-    # 要素の値/全体の要素の合計
-    y = exp_a / sum_exp_a
-
-    return y
 
 # 後処理、出力層での入力->出力
 def onodes_cal(yh, network):
---
+++ common/functions.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# シグモイド関数(活性化関数)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# # ソフトマックス関数
+# def softmax(a):
+#     # 一番大きい値を取得
+#     c = np.max(a)
+#     # 各要素から一番大きな値を引く（オーバーフロー対策）
+#     exp_a = np.exp(a - c)
+#     sum_exp_a = np.sum(exp_a)
+#     # 要素の値/全体の要素の合計
+#     y = exp_a / sum_exp_a
+
+#     return y
+
+# # ソフトマックス関数
+# def softmax(a):
+#     # 一番大きい値を取得
+#     c = np.max(a, axis=1, keepdims=True)
+#     # 各要素から一番大きな値を引く（オーバーフロー対策）
+#     exp_a = np.exp(a - c)
+#     sum_exp_a = np.sum(exp_a, axis=1, keepdims=True)
+#     # 要素の値/全体の要素の合計
+#     y = exp_a / sum_exp_a
+
+#     return y
+
+# ソフトマックス関数
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
+
+    x = x - np.max(x) # オーバーフロー対策
+    return np.exp(x) / np.sum(np.exp(x))
