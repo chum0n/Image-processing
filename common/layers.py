eb00618c95dbecae610d6fb973a60ec670997029
tkt914 <cyumon_tkt.0914@icloud.com>
2020-01-21 21:03:07 +0900

    finish pooling

--- common/layers.py
+++ common/layers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from common.functions import *
+from common.util import *
 
 class Sigmoid:
     def __init__(self):
@@ -183,5 +184,110 @@ class BatchNormalization:
         
         self.dgamma = dgamma
         self.dbeta = dbeta
+        
+        return dx
+
+# 畳み込み層
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+        # 中間データ（backward時に使用）
+        self.x = None   
+        self.col = None
+        self.col_W = None
+        
+        # 重み・バイアスパラメータの勾配
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        # フィルターの個数、チャンネル、高さ、幅
+        FN, C, FH, FW = self.W.shape
+        # 入ってくるデータのバッチ数、チャンネル、高さ、幅
+        N, C, H, W = x.shape
+        # 出ていくデータの高さ
+        out_h = int(1 + (H + 2*self.pad - FH) / self.stride)
+        # 出ていくデータの幅
+        out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
+
+        # フィルターによって都合のいいように入力データを展開
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        # フィルターの展開
+        col_W = self.W.reshape(FN, -1).T # -1指定で前要素数/FNの形にしてくれる
+        out = np.dot(col, col_W) + self.b
+
+        # transpose関数は多次元配列の軸の順番を入れ替える関数
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    # 逆伝播はほぼAffineと同じ
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+
+# プーリング層
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        # 出ていくデータの高さ
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        # 出ていくデータの幅
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        # 展開
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        # チャンネルごとに独立に展開
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        # 行ごと(1次元目の軸ごと)に最大値を求める
+        out = np.max(col, axis=1)
+
+        # 整形
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    # Reluの逆伝播参考
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+        
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,)) 
+        
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
         
         return dx
