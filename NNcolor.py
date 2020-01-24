722b0c9a7826827a971e036511e2875df8a35a7c
tkt914 <cyumon_tkt.0914@icloud.com>
2020-01-24 21:10:01 +0900

    repo3

--- NNcolor.py
+++ NNcolor.py
@@ -10,7 +10,7 @@ INNODES = 3072
 HNODES = 100
 ONODES = 10
 
-ITER_NUM = 20000 # 勾配法による更新の回数
+ITER_NUM = 18000 # 勾配法による更新の回数
 TEACH_NUM = 60000 # 教師データの数
 BATCH_SIZE = 100
 LEARNING_LATE = 0.01
@@ -23,38 +23,38 @@ def unpickleall():
     for i in range(5):
         with open("/Users/daisuke/le4nn/cifar-10-batches-py/data_batch_"+str(i+1), 'rb') as fo:
             dict = pickle.load(fo, encoding='bytes')
-        file_X = np.array(dict[b'data'])
-        file_X = file_X.reshape((file_X.shape[0], -1))
-        file_X = file_X / 255.0 # 0から1までの範囲にする
-        file_Y = np.array(dict[b'labels'])
+        x_train_part = np.array(dict[b'data'])
+        x_train_part = x_train_part.reshape((x_train_part.shape[0], -1))
+        x_train_part = x_train_part / 255.0 # 0から1までの範囲にする
+        t_train_part = np.array(dict[b'labels'])
         if i == 0:
-            X = file_X
-            Y = file_Y
+            x_train = x_train_part
+            t_train = t_train_part
         else:
-            X = np.append(X, file_X, axis=0)
-            Y = np.append(Y, file_Y, axis=0)
-    return X, Y
+            x_train = np.append(x_train, x_train_part, axis=0)
+            t_train = np.append(t_train, t_train_part, axis=0)
+    return x_train, t_train
 
-X,Y = unpickleall()
-x = np.array(X) # (50000, 3*32*32)
-y = np.array(Y) # (50000,)
+x_train,t_train = unpickleall()
+x_train = np.array(x_train) # (50000, 3*32*32)
+t_train = np.array(t_train) # (50000,)
 
 for i in range(ITER_NUM):
-    ran_num = np.random.choice(x.shape[0], BATCH_SIZE)
-    x_batch = x[ran_num, :] # (100, 3*32*32)
-    y_batch = y[ran_num] # (100, )
-    onehot_y_batch = np.eye(10)[y_batch] # (100, 10) 変換元が10種類の場合は、10×10の単位行列を作ってインデックスに変換元の値をいれる
+    ran_num = np.random.choice(x_train.shape[0], BATCH_SIZE)
+    x_batch = x_train[ran_num, :] # (100, 3*32*32)
+    t_batch = t_train[ran_num] # (100, )
+    onehot_t_batch = np.eye(10)[t_batch] # (100, 10) 変換元が10種類の場合は、10×10の単位行列を作ってインデックスに変換元の値をいれる
 
     # 誤差逆伝播法によって勾配を求める
-    grads = network.gradient(x_batch, onehot_y_batch)
+    grads = network.gradient(x_batch, onehot_t_batch)
 
     # 更新
     optimizer.update(network.params, grads)
 
-    loss = network.loss(x_batch, onehot_y_batch)
+    loss = network.loss(x_batch, onehot_t_batch)
 
     if i % ITER_PER_EPOC == 0: # エポック終了時
-        train_acc = network.accuracy(x, y)
+        train_acc = network.accuracy(x_train, t_train)
 
         print(int(i / ITER_PER_EPOC) + 1, ": train_acc, cross_entropy_error |", train_acc*100, "%,",loss)
 
